4c0ea4e399987833a7fa4fe4fccebc4eecaa1660
Emp-Norton <jamesjelenko@gmail.com>
2018-01-10 19:45:44 -0800

    created route for adding bookings

--- server/routers.js
+++ server/routers.js
@@ -2,9 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 const getAllListings = require('./../database/queries/getAllListings.js');
-const getListingsByCity = require('./../database/queries/getListingsByCity.js');
+const checkAvailability = require('./../database/queries/checkAvailability.js')
 
-router.get('*/listings-bryce', (req, res) => getListingsByCity(req.query.city, results => res.send(results)));
+router.get('*/listings-bryce', (req, res) => getAllListings(results => console.log(results)));
+
+router.post('*/bookings-james', (req, res) => {
+			var dates = req.body.data;
+			var listingId = req.body.listing;
+			checkAvailability(listingId, dates, function(results){
+				if (results){
+					console.log(' inserting')
+				} else {
+					console.log('unavailable')
+				}
+			})
+  })
 
 
 
