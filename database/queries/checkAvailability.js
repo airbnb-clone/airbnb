7e9975455833aaf65b5f783d1495ed5d436cc625
Emp-Norton <jamesjelenko@gmail.com>
2018-01-11 18:43:51 -0800

    fix linting errors

--- database/queries/checkAvailability.js
+++ database/queries/checkAvailability.js
@@ -2,33 +2,33 @@
 const connection = require('./../config.js');
 
 const checkAvailability = (listing, dates, callback) => {
-	var queryString = `select * from bookings where listing_id = ` + listing
+  var queryString = 'select * from bookings where listing_id = ' + listing;
 
 
-	connection.query(queryString, (err, results) => {
-		var availability = true;
-		if (err){
-			console.log(err)
-		} else {
-			var bookedDates = [];
-			results.forEach(result => {
-				bookedDates.push(result.dateRented);
-			})
-			var formattedDates = bookedDates.map(bd => bd.toString().split(' ').slice(0, 5).join(' '))
-			dates.forEach(date => {
-				var unixDate = Date.parse(date);
-				formattedDates.forEach(formDate => {
-					var unixFormDate = Date.parse(formDate);
-					if (unixDate == unixFormDate){
-						availability = false;
-					}
-				})
-			})
-			callback(availability);
-		}
-	})
+  connection.query(queryString, (err, results) => {
+    var availability = true;
+    if (err) {
+      console.log(err);
+    } else {
+      var bookedDates = [];
+      results.forEach(result => {
+        bookedDates.push(result.dateRented);
+      });
+      var formattedDates = bookedDates.map(bd => bd.toString().split(' ').slice(0, 5).join(' '));
+      dates.forEach(date => {
+        var unixDate = Date.parse(date);
+        formattedDates.forEach(formDate => {
+          var unixFormDate = Date.parse(formDate);
+          if (unixDate === unixFormDate) {
+            availability = false;
+          }
+        });
+      });
+      callback(availability);
+    }
+  });
 
-}
+};
 
 module.exports = checkAvailability;
 
--- server/routers.js
+++ server/routers.js
@@ -10,18 +10,18 @@ const saveReservation = require('./../database/queries/saveReservation.js');
 router.get('*/listings-bryce', (req, res) => getListingsByCity(req.query.city, results => res.send(results)));
 
 router.post('*/bookings-james', (req, res) => {
-			var dates = req.body.data;
-			var listingId = req.body.listing;
-			var userId = req.body.user;
-			checkAvailability(listingId, dates, function(results){
-				if (results){
-					saveReservation(listingId, userId, dates, function(results){
-					});
-				} else {
-					console.log('failed') // render failures 'already booked' page
-				}
-			})
-  })
+  var dates = req.body.data;
+  var listingId = req.body.listing;
+  var userId = req.body.user;
+  checkAvailability(listingId, dates, function(results) {
+    if (results) {
+      saveReservation(listingId, userId, dates, function(results) {
+      });
+    } else {
+      console.log('failed'); // render failures 'already booked' page
+    }
+  });
+});
 
 router.get('*/listings-ted', (req, res) => getAllListings(results => {
   console.log(results)
