1dbf922a9858ad4f863ecd89a0ba387cf06416db
vgarba1 <vgarba3@gmail.com>
2018-01-12 17:10:22 -0800

    User Component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Search from './components/Search.jsx'
-import UserComponent from './components/UserComponent.jsx'
+import Search from './components/Search.jsx';
+import Listings from './components/listings.jsx'
 import axios from 'axios';
+import {BrowserRouter, Route, hashHistory} from 'react-router-dom';
+import Main from './components/main.jsx'
 
  export default class App extends React.Component {
   constructor(props) {
@@ -12,20 +14,38 @@ import axios from 'axios';
       listings: [],
       bookings: [],
     }
+
     this.search = this.search.bind(this);
   }
 
-  search() {
-    
+  search(city) {
+    axios.get('/listings-bryce', {
+      params: {
+        city: city
+      }
+    })
+    .then(response => this.setState({listings: response.data}))
+    .catch(error => console.log(error))
   }
 
   render() {
     return (
       <div>
         <Search search={this.search} />
+        <Main />
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <BrowserRouter history={hashHistory}>
+    <div>
+      <Route path='/' exact component={App}></Route>
+      <Route path='/user' component={UserComponent}></Route>
+    </div>
+  </BrowserRouter>
+  , document.getElementById('app'));
+
+
+
