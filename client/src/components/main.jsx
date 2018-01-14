49233f0720bd06e96421f7f5e4170924c96fb84e
Bryce Sorensen <26312790+brycessorensen@users.noreply.github.com>
2018-01-13 19:01:57 -0800

    Changes

--- client/src/components/main.jsx
+++ client/src/components/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Listings from './Listings.jsx';
-import ListingEntryDetail from './ListingEntryDetail.jsx';
+import ListingEntryDetail from './ListingEntryDetails.jsx';
 
 
 export default class Main extends React.Component {
