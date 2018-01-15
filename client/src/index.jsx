7f12588f98bfb4b96db48e4c9830ee16aab709c3
Ted Anyansi <tanyansi@gmail.com>
2018-01-15 13:57:13 -0800

    changed a few filenames to uppercase for consistency. Removed see more button while making photo & title of listing clickable instead

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Search from './components/Search.jsx';
-import Listings from './components/listings.jsx'
+import Listings from './components/Listings.jsx'
 import axios from 'axios';
 import {BrowserRouter, Route, BrowserHistory, Link} from 'react-router-dom';
-import Main from './components/main.jsx'
+import Main from './components/Main.jsx'
 import UserComponent from './components/UserComponent.jsx'
 import ListingEntryDetails from './components/ListingEntryDetails.jsx'
 import Navigation from './components/Navigation.jsx';
